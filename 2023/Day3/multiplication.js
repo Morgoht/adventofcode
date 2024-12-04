913b2e0d95cfa5f8858febbd96a69f89a5e4272e
AmauryFlyx <amaury@flyx.cloud>
2024-12-04 08:20:02 +0100

    Day 3

--- 2023/Day3/multiplication.js
+++ 2023/Day3/multiplication.js
@@ -5,19 +5,35 @@ var fs =  require('fs');
 const pathFile = './data.txt';
 
 fs.readFile('./data.txt', 'utf8', (err, data) => {
-
-    var result = 0;
-    const regEx = /mul\((\d{1,3}),(\d{1,3})\)/g;
+    var finalResult = 0;
+    const tab = data.split('do()');
+    console.log(tab);
+    const regEx = /mul\((\d{1,3}),(\d{1,3})\)/g; // prend les mul en tableau
     const regExMul = /\d+/g; // prend les nombre d'une ligne
-    const mulTab = data.match(regEx);
-    console.log(mulTab);
-    mulTab.forEach(line => {
-        console.log('line',line);
-        
-        result += line.match(/\d+/g).map(Number)[0] * line.match(/\d+/g).map(Number)[1];
-
+    tab.forEach(line => {
+        if(!line.match('don\'t()')){
+            console.log('no don\'t in line : ', line);
+            finalResult += calculateLine(line);
+        }
+        else{
+            console.log('line contains don\'t : ', line);
+            finalResult += calculateLine(line.split('don\'t()')[0]);
+        }
     })
-    console.log('result', result);
+
+    function calculateLine(line) {
+        console.log('line in function : ', line);
+        var result = 0;
+        const mulTab = line.match(regEx);
+        console.log(mulTab);
+        mulTab.forEach(line => {
+            result += line.match(/\d+/g).map(Number)[0] * line.match(/\d+/g).map(Number)[1];
+        });
+
+        return result;
+    }
+
+    console.log('result', finalResult);
 
     
 }
